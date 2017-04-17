1787e43f241c959158fc2c18ba8260d62c238f65
Tim Cook <tc@papercloud.com.au>
2017-04-17 17:15:03 +1000

    added categorise and breakdown routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('transactions', function() {
+    this.route('categorise');
+    this.route('breakdown');
+  });
 });
 
 export default Router;
